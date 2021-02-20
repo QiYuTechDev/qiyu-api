686b355909a0c96cd9f5ff39b68092adb947a305
dev <dev@qiyutech.tech>
2021-02-20 22:11:21 +0800

    make happy lint

--- qiyu_api/ztk_api/__init__.py
+++ qiyu_api/ztk_api/__init__.py
@@ -1,39 +1,39 @@
-from .auth_account_args import AuthAccountArgs
-from .bang_dan_tui_jian_args import BangDanTuiJianArgs
-from .bang_dan_tui_jian_model import BangDanTuiJianModel
-from .batch_item_args import BatchItemsArgs
-from .batch_item_model import BatchItemModel
-from .batch_item_resp import BatchItemResp
-from .channel_account_info_args import ChannelAccountInfoArgs
-from .channel_id_list_args import ChannelIdListArgs
-from .channel_invite_code_args import ChannelInviteCodeArgs
-from .channel_save_record_args import ChannelSaveRecordArgs
-from .gao_yong_args import GaoYongArgs
-from .gao_yong_model import GaoYongModel
-from .gao_yong_resp import GaoYongResp
-from .guess_you_like_args import GuessYouLikeArgs
-from .guess_you_like_model import GuessYouLikeModel
-from .guess_you_like_resp import GuessYouLikeResp
-from .item_detail_args import ItemDetailArgs
-from .item_detail_model import ItemDetailModel
-from .item_detail_v2_args import ItemDetailV2Args
-from .item_detail_v2_model import ItemDetailV2Model
-from .item_detail_v2_resp import ItemDetailV2Resp, ItemDetailV2ContentItem
-from .ju_hua_suan_args import JuHuaSuanArgs
-from .ju_hua_suan_model import JuHuaSuanModel
-from .new_order_args import NewOrderArgs
-from .nine_nine_args import NineNineArgs
-from .nine_nine_model import NineNineModel
-from .order_details_resp import OrderDetailsResp, OrderDto, TkStatusEnum
-from .search_args import SearchArgs
-from .search_model import SearchModel
-from .suggest_args import SuggestArgs
-from .tkl_create_args import TKLCreateArgs
-from .tkl_create_resp import TKLCreateResp
-from .tmall_chao_shi_args import TMallChaoShiArgs
-from .tmall_chao_shi_model import TmallChaoShiModel
-from .tmall_shang_pin_args import TMallShangPinArgs
-from .tmall_shang_pin_model import TmallShangPinModel
-from .ztk import ZTK
-from .ztk_std import ZTKStd
-from .ztk_sync import ZTKSync
+from .auth_account_args import AuthAccountArgs  # noqa
+from .bang_dan_tui_jian_args import BangDanTuiJianArgs  # noqa
+from .bang_dan_tui_jian_model import BangDanTuiJianModel  # noqa
+from .batch_item_args import BatchItemsArgs  # noqa
+from .batch_item_model import BatchItemModel  # noqa
+from .batch_item_resp import BatchItemResp  # noqa
+from .channel_account_info_args import ChannelAccountInfoArgs  # noqa
+from .channel_id_list_args import ChannelIdListArgs  # noqa
+from .channel_invite_code_args import ChannelInviteCodeArgs  # noqa
+from .channel_save_record_args import ChannelSaveRecordArgs  # noqa
+from .gao_yong_args import GaoYongArgs  # noqa
+from .gao_yong_model import GaoYongModel  # noqa
+from .gao_yong_resp import GaoYongResp  # noqa
+from .guess_you_like_args import GuessYouLikeArgs  # noqa
+from .guess_you_like_model import GuessYouLikeModel  # noqa
+from .guess_you_like_resp import GuessYouLikeResp  # noqa
+from .item_detail_args import ItemDetailArgs  # noqa
+from .item_detail_model import ItemDetailModel  # noqa
+from .item_detail_v2_args import ItemDetailV2Args  # noqa
+from .item_detail_v2_model import ItemDetailV2Model  # noqa
+from .item_detail_v2_resp import ItemDetailV2Resp, ItemDetailV2ContentItem  # noqa
+from .ju_hua_suan_args import JuHuaSuanArgs  # noqa
+from .ju_hua_suan_model import JuHuaSuanModel  # noqa
+from .new_order_args import NewOrderArgs  # noqa
+from .nine_nine_args import NineNineArgs  # noqa
+from .nine_nine_model import NineNineModel  # noqa
+from .order_details_resp import OrderDetailsResp, OrderDto, TkStatusEnum  # noqa
+from .search_args import SearchArgs  # noqa
+from .search_model import SearchModel  # noqa
+from .suggest_args import SuggestArgs  # noqa
+from .tkl_create_args import TKLCreateArgs  # noqa
+from .tkl_create_resp import TKLCreateResp  # noqa
+from .tmall_chao_shi_args import TMallChaoShiArgs  # noqa
+from .tmall_chao_shi_model import TmallChaoShiModel  # noqa
+from .tmall_shang_pin_args import TMallShangPinArgs  # noqa
+from .tmall_shang_pin_model import TmallShangPinModel  # noqa
+from .ztk import ZTK  # noqa
+from .ztk_std import ZTKStd  # noqa
+from .ztk_sync import ZTKSync  # noqa
